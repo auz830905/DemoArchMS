dc770ed32699ca2e852979777be4f0443c9e98c9
Arley Ulloa <arley.ulloa@ntsprint.com>
2022-10-06 15:10:46 -0400

    Cambios realizados con la api gateway

---
+++ src/APIGateway/Agregators/ClasesPorProfesoresAggregator.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace APIGateway.Agregators
+{
+    public class ClasesPorProfesoresAggregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
+        {
+            var userResponseContext = await responses[0].Items.DownstreamResponse().Content.ReadAsStringAsync();
+            var postResponseContext = await responses[1].Items.DownstreamResponse().Content.ReadAsStringAsync();
+
+            var users = JsonConvert.DeserializeObject<List<User>>(userResponseContext);
+            var posts = JsonConvert.DeserializeObject<List<Post>>(postResponseContext);
+
+            foreach (var user in users)
+            {
+                var userPost = posts.Where(p => p.IdUser == user.Id).ToList();
+                user.Posts.AddRange(userPost);
+            }
+
+            var postsByUserString = JsonConvert.SerializeObject(users);
+
+            var stringContent = new StringContent(postsByUserString)
+            {
+                Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+            };
+
+            return new DownstreamResponse(stringContent, HttpStatusCode.OK, new List<KeyValuePair<string, IEnumerable<string>>>(), "OK");
+        }
+    }
+}
--- src/APIGateway/Program.cs
+++ src/APIGateway/Program.cs
@@ -1,13 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot()
+    .AddSingletonDefinedAggregator<UserPostAggregator>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,28 +22,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseOcelot().Wait();
+
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
