63a2639864b9aac1e11a0792ad70161f6efc0433
Arley Ulloa <arley.ulloa@ntsprint.com>
2022-10-10 15:39:30 -0400

    Update endpoint routs for app client

--- src/WebApp/Static/Endpoints.cs
+++ src/WebApp/Static/Endpoints.cs
@@ -5,13 +5,18 @@
         private static string BaseUrl = "https://localhost:7119";
 
         public static string ProfesoresList = $"{BaseUrl}/gateway/profesores";
+        public static string Profesor = $"{BaseUrl}/gateway/profesores/";
         public static string AddProfesor = $"{BaseUrl}/gateway/profesores";
         public static string DeleteProfesor = $"{BaseUrl}/gateway/profesores/";
+        public static string UpdateProfesor = $"{BaseUrl}/gateway/profesores";
 
         public static string ClasesList = $"{BaseUrl}/gateway/clases";
+        public static string Clase = $"{BaseUrl}/gateway/clases/";
         public static string AddClase = $"{BaseUrl}/gateway/clases";
         public static string DeleteClase = $"{BaseUrl}/gateway/clases/";
+        public static string UpdateClase = $"{BaseUrl}/gateway/clases";
 
+        public static string ClasesNotAssinedProfesor = $"{BaseUrl}/gateway/clasesprofesores/";
         public static string ClasesByProfesor = $"{BaseUrl}/gateway/clasesprofesores/";
         public static string AddClaseProfesor = $"{BaseUrl}/gateway/clasesprofesores/";
         public static string DeleteClasesProfesor = $"{BaseUrl}/gateway/clasesprofesores/";
