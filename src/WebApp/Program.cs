756bf68ca2799a7e345c6a0a3a2f214d66bc87db
Arley Ulloa <arley.ulloa@ntsprint.com>
2022-10-11 20:04:26 -0400

    Add config to toastr notification

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Blazored.Toast;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebApp;
 
@@ -7,6 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredToast();
 
 await builder.Build().RunAsync();
 
