ad515f0aca1d55e281a77edb3751e68c3eba0c18
Arley Ulloa <arley.ulloa@ntsprint.com>
2022-10-12 12:51:46 -0400

    Add annotations for validate in view

--- src/Shared/DTOs/Clase.cs
+++ src/Shared/DTOs/Clase.cs
@@ -1,8 +1,11 @@
-﻿namespace Shared.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTOs
 {
     public class Clase
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre de la clase es obligatorio")]
         public string Nombre { get; set; } = null!;
         public virtual Profesor Profesor { get; set; } = null!;
     }
--- src/Shared/DTOs/Profesor.cs
+++ src/Shared/DTOs/Profesor.cs
@@ -1,9 +1,16 @@
-﻿namespace Shared.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTOs
 {
     public class Profesor
     {
+        [Required(ErrorMessage = "El Carné de identidad es obligatorio")]
+        [StringLength(maximumLength: 11, ErrorMessage = "Carné de identidad no válido. Tiene que tener 11 caracteres numéricos", MinimumLength = 11)]
+        [RegularExpression(@"\d*", ErrorMessage = "Solo admite números el Carné de identidad")]
         public string CI { get; set; } = null!;
+        [Required(ErrorMessage = "El nombre del profesor es obligatorio")]
         public string Nombre { get; set; } = null!;
+        [Required(ErrorMessage = "Los Apellidos del profesor son obligatorios")]
         public string Apellidos { get; set; } = null!;
         public List<Clase> Clases { get; set; } = null!;
     }
