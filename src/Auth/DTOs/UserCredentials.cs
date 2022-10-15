2b0d5718693691a747766a90972102614220c236
Arley Ulloa <arley.ulloa@ntsprint.com>
2022-10-15 12:41:42 -0400

    Dtos for authenticated and get token

---
+++ src/Auth/DTOs/UserCredentials.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Auth.DTOs
+{
+    public class UserCredentials
+    {
+        [Required(ErrorMessage = "El correo es requerido")]
+        [StringLength(maximumLength: 256)]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Correo no válido")]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [StringLength(maximumLength: 250, ErrorMessage = "La contraseña debe tener mínimo 8 caracteres ", MinimumLength = 8)]
+        public string Password { get; set; } = null!;
+    }
+}
