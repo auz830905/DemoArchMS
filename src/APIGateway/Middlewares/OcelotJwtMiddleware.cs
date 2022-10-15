0e20688fc50acf823953a338e3c4ea2933dca5f3
Arley Ulloa <arley.ulloa@ntsprint.com>
2022-10-15 12:35:20 -0400

    MiddleWare for authorized based in claims

---
+++ src/APIGateway/Middlewares/OcelotJwtMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Ocelot.Middleware;
+
+namespace APIGateway.Middlewares
+{
+    public class OcelotJwtMiddleware
+    {
+        public static bool Authorize(HttpContext ctx)
+        {
+            if (ctx.Items.DownstreamRoute().AuthenticationOptions.AuthenticationProviderKey != null)
+            {
+                var Claims = ctx.User.Claims.Where(claim => claim.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role")
+                                            .OrderBy(claim => claim.Value)
+                                            .Select(claim => claim.Value)
+                                            .ToArray();
+
+                var required = ctx.Items.DownstreamRoute().RouteClaimsRequirement;
+
+                var Roles = required["Role"].Split(",")
+                                            .Select(r => r.Trim())
+                                            .OrderBy(r => r)
+                                            .ToArray();
+
+                var interception = (from claim in Claims.Intersect(Roles)
+                                    select claim).ToList();
+
+                return interception.Count != 0;
+            }
+            return true;            
+        }
+    }
+}
