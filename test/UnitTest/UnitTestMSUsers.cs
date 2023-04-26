44903c5bedbbe1cd61b144a4bc7f6c964a0c21c4
Arley Ulloa <arley.ulloa@ntsprint.com>
2023-04-26 12:14:57 -0400

    Update test

--- test/UnitTest/UnitTestMSUsers.cs
+++ test/UnitTest/UnitTestMSUsers.cs
@@ -8,13 +8,12 @@ using MSUsers.Interfaces;
 using FluentAssertions;
 using UnitTest.Extensions;
 
-
 namespace UnitTest;
 
 public class UnitTestMSUsers
 {
     [Fact]
-    public async void EndPointsExtensions_CreateUser_WhenPasswordIsNull_ReturnsBadReques()
+    public async void EndPointsExtensions_CreateUser_WhenPasswordIsNull_ReturnsBadRequest()
     {
         //Arrange
         var (mockLogger, mockRepository) = GetDependencies();
@@ -26,11 +25,68 @@ public class UnitTestMSUsers
         }; 
 
         //Act
-        var response = await EndPointsExtensions.CreateUser(user, mockRepository.Object, mockLogger.Object);
+        var response = (BadRequest<object>) await EndPointsExtensions.CreateUser(user, mockRepository.Object, mockLogger.Object);
 
         //Assert
-        response.GetBadRequestObjectResultStatusCode().Should().Be(StatusCodes.Status400BadRequest);
+        Assert.Equal(400, response.StatusCode);
     }
+
+    [Fact]
+    public async void EndPointsExtensions_CreateUser_WhenUserNameIsNull_ReturnsBadRequest()
+    {
+        //Arrange
+        var (mockLogger, mockRepository) = GetDependencies();
+
+        var user = new UserCredentials()
+        {
+            Email = null!,
+            Password = "123456"
+        };
+
+        //Act
+        var response = (BadRequest<object>) await EndPointsExtensions.CreateUser(user, mockRepository.Object, mockLogger.Object);
+
+        //Assert
+        Assert.Equal(400, response.StatusCode);
+    }
+
+    [Fact]
+    public async void EndPointsExtensions_CreateUser_WhenPasswordIsEmpty_ReturnsBadRequest()
+    {
+        //Arrange
+        var (mockLogger, mockRepository) = GetDependencies();
+
+        var user = new UserCredentials()
+        {
+            Email = "arley@arley.com",
+            Password = string.Empty
+        };
+
+        //Act
+        var response = (BadRequest<object>)await EndPointsExtensions.CreateUser(user, mockRepository.Object, mockLogger.Object);
+
+        //Assert
+        Assert.Equal(400, response.StatusCode);
+    }
+
+    [Fact]
+    public async void EndPointsExtensions_CreateUser_WhenUserNameIsEmpty_ReturnsBadRequest()
+    {
+        //Arrange
+        var (mockLogger, mockRepository) = GetDependencies();
+
+        var user = new UserCredentials()
+        {
+            Email = string.Empty,
+            Password = "123456"
+        };
+
+        //Act
+        var response = (BadRequest<object>)await EndPointsExtensions.CreateUser(user, mockRepository.Object, mockLogger.Object);
+
+        //Assert
+        Assert.Equal(400, response.StatusCode);
+    }    
 
     private static (Mock<ILogger<IUsersRepository>>, Mock<IUsersRepository>) GetDependencies()
         => (new(), new());
